d481f7a749eaf744831bdf792f3ea07ce5e00685
Paul <Paul@VAIO>
2018-02-12 12:32:47 +0000

    Games List Controller adjusted for dynmic routing. Claims Controller Added

---
+++ AchievevementWebAPIExample/Controllers/ClaimsController.cs
@@ -0,0 +1,41 @@
+﻿using AchievevementWebAPIExample.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading;
+using System.Web.Http;
+
+namespace AchievevementWebAPIExample.Controllers
+{
+    [Authorize]
+    public class ClaimsController : ApiController
+    {
+        ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public dynamic getClaims()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+
+            var userClaim = claims
+                            .Where(c => c.Type == ClaimTypes.Name);
+            return userClaim;
+        }
+    }
+}
--- AchievevementWebAPIExample/Controllers/GamesListController.cs
+++ AchievevementWebAPIExample/Controllers/GamesListController.cs
@@ -8,13 +8,25 @@ using System.Web.Http;
 
 namespace AchievevementWebAPIExample.Controllers
 {
+    [RoutePrefix("api/games")]
     public class GamesListController : ApiController
     {
         ApplicationDbContext db = new ApplicationDbContext();
-
+        [Route("ListGames")]
         public dynamic getGameList()
         {
             return db.Games.ToList();
         }
+
+        [Authorize]
+        [Route("GamerInfo")]
+        public dynamic getGamerInfo()
+        {
+            var info =  db.Users.Where(u => u.UserName == User.Identity.Name)
+                    .Select(p => new { p.GamerTag, p.XP });
+            return info;
+            
+        }
+
     }
 }
